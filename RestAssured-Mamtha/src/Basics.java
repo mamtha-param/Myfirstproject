9390d46ea8f7cea39e30a47437835a050c7c6be6
mamtha-param <mamthadgowda409@gmail.com>
2023-01-17 19:57:05 +0530

    first commit

---
+++ RestAssured-Mamtha/src/Basics.java
@@ -0,0 +1,15 @@
+
+public class Basics {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//validate if Add place Api is working as expected 
+		//3 basic prnples of Rest asssured
+		//Given-All inputs
+		//When-Submit the API 
+		//then-Validate the response
+		
+		RestAssured.
+	}
+
+}
